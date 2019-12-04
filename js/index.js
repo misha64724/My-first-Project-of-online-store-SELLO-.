0bdbfc4eae90628dee0404ca1b631e3096f8a8a9
misha64724 <misha64724@gmail.com>
2019-12-04 15:12:52 +0200

    First commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+function changeBckColor1() {   
+    document.body.style.backgroundColor= "#F9F9F9";
+    document.getElementById("change-bckgr-color-navigation__header").style.backgroundColor = "#F9F9F9";
+};
+function changeBckColor2() {   
+    document.getElementById("change-bckgr-color-body").style.backgroundColor = "#F1DBD7";
+    document.getElementById("change-bckgr-color-navigation__header").style.backgroundColor = "#F1DBD7";
+}
+
